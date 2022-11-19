eae874299b5153a629551b22b91240caac301d64
Mohamed Kilany <Mahammadkelani92@gmail.com>
2022-11-19 19:18:33 +0200

    Change DataProviders.java from Static Data to Excel Reader

--- src/test/java/Utils/DataProviders.java
+++ src/test/java/Utils/DataProviders.java
@@ -1,15 +1,30 @@
 package Utils;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.DataProvider;
+
+import java.io.IOException;
 
 public class DataProviders {
     // This one is providing a single String as a search term
     // We can use an Excel reader or File CSV reader inside it to get data from outsource
-    // For this example we use static data
+    // For this example we use Excel Reader
+
     @DataProvider(name = "searchTermsProvider")
-    public Object[][] Data() {
-        return new Object[][]{
-                {"Adam"}, {"Help"}, {"Free"}
-        };
+    public Object[][] Data() throws IOException {
+        String[][] GetData;
+        XSSFWorkbook workbook = new XSSFWorkbook(System.getProperty("user.dir") + "\\src\\Terms.xlsx");
+        XSSFSheet sheet = workbook.getSheet("Terms");
+        int rows = sheet.getLastRowNum() + 1;
+        int columns = sheet.getRow(0).getLastCellNum();
+        GetData = new String[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                GetData[i][j] = sheet.getRow(i).getCell(j).toString();
+            }
+        }
+        return GetData;
     }
+
 }
